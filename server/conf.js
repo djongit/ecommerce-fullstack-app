320f2dc8177a76fc476b665c2c35b20a028adf67
unknown <djongit@hotmail.com>
2024-01-10 17:06:56 +0000

    README updatated, config file references altered

--- server/conf.js
+++ server/conf.js
@@ -1,7 +1,7 @@
 
 const path = require('path');
-require('dotenv').config({path: path.join(__dirname, '.env')}); // explicitly attach .env file. 
-
+// require('dotenv').config({path: path.join(__dirname, '.env')}); // explicitly attach .env file. 
+require('doenv').config();
 module.exports = {
     PORT: process.env.PORT,
     DB: {
